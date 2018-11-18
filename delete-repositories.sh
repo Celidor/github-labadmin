a42035dbcd3c24757c02efd87fb0def517270865
Paul Schwarzenberger <paul@celidor.net>
2018-11-18 08:48:53 +0000

    added sleep warning

--- delete-repositories.sh
+++ delete-repositories.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# USE WITH CARE - WILL DELETE ALL GITHUB REPOSITORIES IN USER ACCOUNT
+# USE WITH CARE - WILL DELETE ALL PUBLIC GITHUB REPOSITORIES IN USER ACCOUNT
 #set github variables from file access-tokens.sh using format
 #NAMEPREFIX="your-prefix"
 #API_TOKEN_1="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 #API_TOKEN_2="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 source access-tokens.sh
-for i in `seq 1 3`;
+for i in `seq 1 14`;
 do
   echo "looking for repositories in github.com/$NAMEPREFIX$i"
   API_TOKEN="API_TOKEN_$i"  
@@ -18,6 +18,8 @@ do
     REPONAME=${REPO#"git@github.com:"}
     REPONAME=${REPONAME%".git"}
     echo "deleting repository" $REPONAME
+    echo "you have 5 seconds to Ctrl-C"
+    sleep 5
     curl -vL \
       -H "Authorization: token $API_TOKEN" \
       -H "Content-Type: application/json" \
